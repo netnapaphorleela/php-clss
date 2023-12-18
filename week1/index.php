b4967e5a8314dcbe5d5a242f602cf93b21a112a9
netnapa2006 <154107533+netnapaphorleela@users.noreply.github.com>
2023-12-18 11:15:06 +0700

    เพิ่มไฟล์ week1

---
+++ week1/index.php
@@ -0,0 +1,14 @@
+<?php
+$address = "moo.3 wattananwong";
+$char1 = 'A';
+echo $address[0].$address[1].$address[2].$address[3].$address[4];
+echo "<br>";
+echo $char1;
+$age = 17;
+$pi = 3.14;
+$pi = 3.14214567;
+
+echo $age+$pi;
+$isTrue = false;
+echo $isTrue;
+?>
